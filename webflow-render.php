b02222039ec88b541b7fcf91183aea990c78965a
rchipka <rchipka@vibethink.com>
2018-12-10 17:51:42 -0500

    v0.2.2

--- webflow-render.php
+++ webflow-render.php
@@ -1,6 +1,8 @@
 <?php
-function webflow_render($keys, $data = null) {
-  if ($data) {
+function webflow_render($keys, $data = null, $callback = null) {
+  if (is_callable($data)) {
+    $callback = $data;
+  } else if (is_array($data)) {
     $item = $data['item'] ?: $data['post'];
 
     if ($item) {
@@ -22,7 +24,11 @@ function webflow_render($keys, $data = null) {
     $GLOBALS['wf_context'] = $data;
   }
 
-  do_action('webflow_render', $keys);
+  do_action('before_webflow_render', $keys);
+
+  do_action('webflow_render', $keys, $callback);
+
+  do_action('after_webflow_render', $keys);
 }
 
 function webflow_init($jsonFile) {
@@ -72,7 +78,7 @@ function webflow_init($jsonFile) {
 
   $GLOBALS['wf_element_index'] = [];
 
-  add_action('webflow_render', function ($keys) use ($elements, $twig) {
+  add_action('webflow_render', function ($keys, $callback) use ($elements, $twig) {
     $max_similarity = 0;
     $matches = [];
 
@@ -146,6 +152,11 @@ function webflow_init($jsonFile) {
 
     $render_context->enter();
 
+    ob_start();
+
+    $slots = (object) [];
+    context()->set('slots', $slots);
+
     try {
       // extract($GLOBALS['wf_context']);
       echo eval('?>' . $twig->render($element->class, $GLOBALS['wf_context']));
@@ -153,11 +164,53 @@ function webflow_init($jsonFile) {
       context()->log($e->getMessage());
     }
 
+    $output = ob_get_clean();
+
+    if (is_callable($callback)) {
+      call_user_func($callback);
+    }
+
+    foreach (get_object_vars($slots) as $name => $slot) {
+      do_action('webflow_slot', $slot, $name);
+      do_action('webflow_slot_' . $name, $slot, $name);
+      
+      $regex = '/<!-- slot-' . $slot->hash . ' -->/';
+      $chunks = array_chunk($slot->data, ceil(sizeof($slot->data) / $slot->total));
+
+      foreach ($chunks as $array) {
+        $output = preg_replace($regex, implode(' ', $array), $output, 1);
+      }
+    }
+
+    if ($element->slot && context()->parent) {
+      context()->parent->get('slots')->{$element->slot}->data[] = $output;
+    } else {
+      echo $output;
+    }
+
     $render_context->exit();
     
     $element->rendering = false;
 
     $GLOBALS['wf_element_index'][$key]++;
-  }, 5, 1);
+  }, 5, 2);
+}
+
+function webflow_slot($name) {
+  $slots = context()->get('slots');
+
+  if (!$slots->{$name}) {
+    $slots->{$name} = (object) [
+      'hash' => md5(rand()),
+      'total' => 0,
+      'data' => [],
+    ];
+  }
+
+  $slots->{$name}->total++;
+
+  // We use a hash so that the slot is unique to the current context
+
+  return '<!-- slot-' . $slots->{$name}->hash . ' -->';
 }
 
